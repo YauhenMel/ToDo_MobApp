86cccda3ab300870712637a56690b2118e02f171
Yauhen Melnik <yauhenmelnik@gmail.com>
2025-06-03 19:29:26 +0300

    init: Create initial project

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green",
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: "900"
+    }
+});
