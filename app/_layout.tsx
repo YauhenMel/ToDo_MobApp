7cf7684f87fc5f1d2ea6f1a5dc5363d3044657f7
Yauhen Melnik <yauhenmelnik@gmail.com>
2025-06-05 02:09:30 +0300

    feat: Create add-task page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,19 +5,23 @@ import AddTask from "@/app/add-task";
 import Task from "@/app/task";
 import {Header} from "@/components/Header";
 import {ROUTES} from "@/constants/routes";
-import {Platform, StatusBar} from "react-native";
+import {Platform, StatusBar } from "react-native";
 import {COLORS} from "@/style/colors";
 
 const Stack = createStackNavigator();
 
 export default function RootLayout() {
   return (
-      <Stack.Navigator initialRouteName={ROUTES.root} screenOptions={{header: Header,
-          cardStyle: {
-              backgroundColor: COLORS.white,
-              paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-          },
-      }}>
+      <Stack.Navigator
+          initialRouteName={ROUTES.root}
+          screenOptions={{
+              header: Header,
+              cardStyle: {
+                  backgroundColor: COLORS.white,
+                  paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+              },
+          }}
+      >
         <Stack.Screen name={ROUTES.root} component={Root} />
         <Stack.Screen name={ROUTES.task} component={Task} />
         <Stack.Screen name={ROUTES.add_task} component={AddTask} />
