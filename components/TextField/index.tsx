00d93f26289752737ff25e8d951b2b496b932c0f
Yauhen Melnik <yauhenmelnik@gmail.com>
2025-06-05 01:01:37 +0300

    refactor: Extend TextField component to use as multiline input

--- components/TextField/index.tsx
+++ components/TextField/index.tsx
@@ -3,10 +3,10 @@ import {Text, TextInput, View} from "react-native";
 import { styles } from "./styles";
 import {TextFieldProps} from "@/components/TextField/types";
 
-export const TextField:FC<TextFieldProps> = ({ label, onChange, value, isTouched, error, isSubmitted, onBlur}) => (
+export const TextField:FC<TextFieldProps> = ({ label, onChange, value, isTouched, error, isSubmitted, onBlur, multiline}) => (
         <View>
             <Text>{label}</Text>
-            <TextInput style={styles.input} onChangeText={onChange} value={value} onBlur={onBlur}/>
+            <TextInput style={styles.input} multiline={multiline} numberOfLines={4} onChangeText={onChange} value={value} onBlur={onBlur}/>
             {((isSubmitted || isTouched) && error) && (<View>
                 <Text style={styles.error}>{error.message}</Text>
             </View>)
