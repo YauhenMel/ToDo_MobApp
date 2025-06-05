59cc9d82cde165efa1b718bda57bc335386d6b08
Yauhen Melnik <yauhenmelnik@gmail.com>
2025-06-05 17:05:22 +0300

    feat: Add put request to change task status

--- app/task/index.tsx
+++ app/task/index.tsx
@@ -1,34 +1,65 @@
 import { View, Text } from 'react-native';
 import { Button } from '@/components/Button';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { formatDate } from '@/utils/formatDate';
 import { styles } from '@/app/task/styles';
-
-const task = {
-  id: 1,
-  title: 'Task 1',
-  description:
-    'Task 1 sijsdids idsidi sdjisdj dsjijdis Task 1djisdijd dsijdijsid ijdsijdisj',
-  status: 'canceled',
-  executionTime: new Date('2020-11-25T12:37'),
-};
+import { useRoute } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { ITask, StatusType } from '@/types';
+import { changeTaskStatus, getTask } from '@/API';
 
 export default function Task() {
+  const [task, setTask] = useState<ITask | null>(null);
+
+  const route = useRoute();
+
+  const taskId = route.params?.id;
+
+  useEffect(() => {
+    if (taskId) {
+      getTask(taskId).then((task) => setTask(task));
+    }
+  }, [taskId]);
+
+  const handlePressChangeTaskStatus = async (
+    taskId: string,
+    status: StatusType,
+  ) => {
+    try {
+      await changeTaskStatus(taskId, status);
+
+      setTask((prevState) => prevState && { ...prevState, status });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView edges={['left', 'right', 'bottom']} style={styles.container}>
-      <View style={styles.task}>
-        <Text style={styles.title}>{task.title}</Text>
-        <Text style={styles.executionTime}>
-          {formatDate(task.executionTime)}
-        </Text>
-        <Text style={styles.description}>{task.description}</Text>
-      </View>
-      <View style={styles.actions}>
-        <Button onPress={() => console.log('Cancel')}>Cancel</Button>
-        <Button filled onPress={() => console.log('Completed')}>
-          Completed
-        </Button>
-      </View>
+      {task && (
+        <>
+          <View style={styles.task}>
+            <Text style={styles.title}>{task.title}</Text>
+            <Text style={styles.executionTime}>
+              {task.executionTime.toString()}
+            </Text>
+            <Text style={styles.description}>{task.description}</Text>
+            <Text>{task.status}</Text>
+          </View>
+          <View style={styles.actions}>
+            <Button
+              onPress={() => handlePressChangeTaskStatus(task.id, 'canceled')}
+            >
+              Cancel
+            </Button>
+            <Button
+              filled
+              onPress={() => handlePressChangeTaskStatus(task.id, 'completed')}
+            >
+              Completed
+            </Button>
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
 }
